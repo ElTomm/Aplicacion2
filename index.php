e471c6d3cc4d507b8ccb6e52138f34b94f9d0572
José04 <77713217+ElTomm@users.noreply.github.com>
2021-07-14 20:21:46 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>LOGIN</title>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <input name="usuario" type="text" h1="Escribe tu nombre de usuario">
+            <br><br>
+            <input name="contraseña" type="password">
+            <br><br>
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </body>
+    </html>
